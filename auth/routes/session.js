869f2e97145039f60fdd49508fd56a12057d8382
chris <kristina.beidina@gmail.com>
2022-04-05 19:51:18 +0300

    feat add registration feat add loggin in feat add getting user

--- auth/routes/session.js
+++ auth/routes/session.js
@@ -2,28 +2,72 @@ const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 
-// where to pass id of the resource in DELETE
+const User = require("../models/user");
+const crypto = require("crypto");
 
-// where to pass credentials in POST
+router.post("/", async (req, res) => {
+  const { login, password } = req.body;
 
-// creates new session aka loggin in
-router.post("/", (req, res) => {
-  // extracting data from request object
+  const user = await User.findOne({ where: { login } });
+  if (!user) {
+    res
+      .status(401)
+      .send({ message: "identification error", error: "no such user" });
+  }
 
-  // identification
+  const hash = crypto
+    .pbkdf2Sync(password, user.salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+  if (user.password != hash) {
+    res
+      .status(401)
+      .send({ message: "authentication error", error: "incorrect password" });
+    return;
+  }
 
-  // authentication
-
-  const token = "123";
-  res.status(200).send({ access_token: "token" });
-  res.status(401).send("error: unauthorized");
+  const head = Buffer.from(
+    JSON.stringify({ alg: "HS256", typ: "jwt" })
+  ).toString("base64");
+  const body = Buffer.from(
+    JSON.stringify({ id: user.id, login: user.login })
+  ).toString("base64");
+  const signature = crypto
+    .createHmac("SHA256", process.env.SECRET_KEY)
+    .update(`${head}.${body}`)
+    .digest("base64");
+  const access_token = `${head}.${body}.${signature}`;
+  res.status(200).send({ message: "successfully logged in", access_token });
 });
 
-// deletes current session aka loggin out
-router.delete("/:id", (req, res) => {
+function authentication(req, res, next) {
+  try {
+    const tokenParts = req.headers.authorization.split(" ")[1].split(".");
+    let signature = crypto
+      .createHmac("SHA256", process.env.SECRET_KEY)
+      .update(`${tokenParts[0]}.${tokenParts[1]}`)
+      .digest("base64");
+
+    if (signature === tokenParts[2]) {
+      req.user_id = JSON.parse(
+        Buffer.from(tokenParts[1], "base64").toString("utf8")
+      ).id;
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({ message: "error: unauthorized" });
+  }
+}
+
+router.delete("/:id", authentication, (req, res) => {
   const { id } = req.url;
-  res.status(201).send("successfully logged out");
+  console.log("id is ", id);
+  console.log("kinda loggin out..........");
+  // User.destroy({where:{id}}).then(()=>{
+  //   res.status(201).send("successfully logged out");
+  // })
   //  res.status(401).send('error: unauthorized');
+  res.status(200).send({ message: "kinda logged out" });
 });
 
 module.exports = router;
--- auth/routes/user.js
+++ auth/routes/user.js
@@ -1,34 +1,50 @@
 const express = require("express");
-const router = express.Router();
 require("dotenv").config();
-const db = require("../db");
+const router = express.Router();
+
 const User = require("../models/user");
+
 const crypto = require("crypto");
 
-// need mysql db and sequelize
-// also need something to hash password with
-
-router.post("/", (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const { body } = req;
-    const { login, password } = body;
+    const { id } = req.params;
+    const user = await User.findOne({ where: { id } });
+
+    if (user) {
+      res.status(200).send({ id: user.id, login: user.login });
+    } else {
+      res.status(404).send({ message: "not found" });
+    }
+  } catch (err) {
+    res.status(500).send({ message: "server error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { login, password } = req.body;
 
     salt = crypto.randomBytes(16).toString("hex");
-    // Hashing user's salt and password with 1000 iterations,
     hash = crypto
-      .pbkdf2Sync(password, this.salt, 1000, 64, `sha512`)
+      .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
       .toString(`hex`);
 
-    User.create({ login, password: hash });
-    // check if exists / try add as if unique
-    // 409 conflict  in case of false result of checking or exceptin
-    // 201 in case of success
-    res
-      .status(409)
-      .send("error: login has to be unique and yours already occupied");
-    res.status(201).send("successfully registered");
-  } catch (e) {
-    console.error(e);
+    User.create({ login, password: hash, salt })
+      .then((userDoc) => {
+        res.status(201).send({
+          message: "successfully registered",
+          user: { id: userDoc.id, login: userDoc.login },
+        });
+      })
+      .catch((err) => {
+        res.status(409).send({
+          message: "bad request",
+          error: "login has to be unique and yours already occupied",
+        });
+      });
+  } catch (error) {
+    res.status(500).send({ message: "server error", error });
   }
 });
 
